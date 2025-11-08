5b82a4b89e19087ab062487c87ebac14ea78822d
Tuna Kodal <tkodal@etu.edu.tr>
2025-11-09 01:47:57 +0300

    Additional methods implemented

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,154 @@
 public class Main {
 
-    public LinkedPositionalList<Watcher> watcher_list = new LinkedPositionalList<>();
-    public LinkedQueue<Incident> incident_queue = new LinkedQueue<>();
-    public LinkedPositionalList<Incident> severity_order_list = new LinkedPositionalList<>();
-    String watcherFile;
-    String incidentFile;
-    boolean processAll;
+    public static LinkedPositionalList<Watcher> watcher_list = new LinkedPositionalList<>();
+    public static LinkedQueue<Incident> incident_queue = new LinkedQueue<>();
+    public static LinkedPositionalList<Incident> severity_order_list = new LinkedPositionalList<>();
+    public String watcherFile;
+    public String incidentFile;
+    public static boolean processAll;
 
     public Main(String watcherFile, String incidentFile, boolean processAll) {
         this.watcherFile = watcherFile;
         this.incidentFile = incidentFile;
-        this.processAll = processAll;
+        Main.processAll = processAll;
     }
 
     public void run(){
 
     }
+
+    public static Watcher createWatcher(String line){
+        String[] arguments = line.trim().split("\\s+");
+        int time = Integer.parseInt(arguments[0]);
+        float latitude = Float.parseFloat(arguments[2]);
+        float longitude = Float.parseFloat(arguments[3]);
+        String name = arguments[4];
+        System.out.println(name + " is added to the watcher-list");
+        return new Watcher(name, latitude, longitude, time);
+    }
+
+    public static Watcher deleteWatcher(String line) {
+        String[] arguments = line.trim().split("\\s+");
+        String name = arguments[2];
+        for (Position<Watcher> p : watcher_list.positions()){
+            if (p.getElement().getName().equals(name)) {
+                System.out.println(name + " is removed from the watcher-list");
+                return watcher_list.remove(p);
+            }
+        }
+        return null;
+    }
+
+    public static void queryHighest(){
+        if (severity_order_list.isEmpty()) {
+            System.out.println("No incidents");
+        }
+        else{
+            Incident highest = severity_order_list.first().getElement();
+            System.out.println("Most severe health incident in past 6 hours:");
+            System.out.println("(Disease: " + highest.getDisease() + ") Severity: " + highest.getSeverity() + " at " + highest.getLocation());
+        }
+    }
+
+    public static void queryDisease(String disease){
+        for (Position<Incident> p : severity_order_list.positions()) {
+            if (p.getElement().getDisease().equals(disease)) {
+                Incident incident = p.getElement();
+                System.out.println("(Disease: " + incident.getDisease() + ") Severity: " + incident.getSeverity() + " at " + incident.getLocation());
+            }
+        }
+    }
+
+    public static void queryRegion(float latitude, float longitude, int radius){
+        for (Position<Incident> p : severity_order_list.positions()) {
+            Incident incident = p.getElement();
+            float lat2 = incident.getLatitude();
+            float lon2 = incident.getLongitude();
+            if (isInRegion(latitude, longitude, lat2, lon2, radius)) {
+                System.out.println("(Disease: " + incident.getDisease() + ") Severity: " + incident.getSeverity() + " at " + incident.getLocation());
+            }
+        }
+
+    }
+
+    public static boolean isInRegion(float lat1, float lon1, float lat2, float lon2, int radius){
+        float x = lat1 - lat2;
+        float y = lon1 - lon2;
+        double distance = Math.sqrt(x*x + y*y);
+        return distance <= radius;
+    }
+
+
+    //========= Incident Methods =========
+
+    public static Incident createIncident(String line) {
+        String[] arguments = line.trim().split("\\s+");
+
+        int time = Integer.parseInt(arguments[0]);
+        String disease = arguments[1];
+        float latitude = Float.parseFloat(arguments[2]);
+        float longitude = Float.parseFloat(arguments[3]);
+        String location = arguments[4];
+        float infectionRate = Float.parseFloat(arguments[5]);
+        int populationAffected = Integer.parseInt(arguments[6]);
+        float severity = Float.parseFloat(arguments[7]);
+        String reportingAgency = arguments[8];
+
+        if (processAll) {
+            System.out.println("(Disease: " + disease + ") at " + location + " is inserted into incident-queue");
+        }
+
+        return new Incident(
+                time, disease, latitude, longitude,
+                location, infectionRate, populationAffected,
+                severity, reportingAgency
+        );
+    }
+
+    public static void updateIncidentQueue(int currentTime) {
+        if (!incident_queue.isEmpty() && currentTime - incident_queue.first().getTime() >= 6) {
+            updateSeverityOrderList(incident_queue.first().getSeverityPosition());
+            incident_queue.dequeue();
+        }
+    }
+
+    private static void updateSeverityOrderList(Position<Incident> pos) {
+        severity_order_list.remove(pos);
+    }
+
+    public static void addIncident(Incident incident) {
+
+        incident_queue.enqueue(incident);
+
+        Position<Incident> newPos = null;
+        if (severity_order_list.isEmpty()) {
+            newPos = severity_order_list.addFirst(incident);
+        } else {
+            for (Position<Incident> currentPos : severity_order_list.positions()) {
+                if (incident.getSeverity() > currentPos.getElement().getSeverity()) {
+                    newPos = severity_order_list.addBefore(currentPos, incident);
+                    break;
+                }
+            }
+            if (newPos == null) {
+                newPos = severity_order_list.addLast(incident);
+            }
+        }
+        incident.setSeverityPosition(newPos);
+    }
+
+    public static void notifyWatchers(Incident incident){
+        for (Position<Watcher> p : watcher_list.positions()) {
+            Watcher watcher = p.getElement();
+            float x = watcher.getLatitude() - incident.getLatitude();
+            float y = watcher.getLongitude() - incident.getLongitude();
+            double distance = Math.sqrt(x*x + y*y);
+            if (distance <= 2 * incident.getSeverity()) {
+                System.out.println("(Disease: " + incident.getDisease() + ") at " + incident.getLocation() + " is close to " + watcher.getName());
+            }
+        }
+    }
+
+
 
 }
